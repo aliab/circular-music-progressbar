2f1ea585b421f039b330974750156dfd787f83db
ali abdolahi <i@abdolahi.info>
2023-05-03 09:54:57 +0330

    upgrade sample app api level to 33

--- library/src/main/java/info/abdolahi/CircularMusicProgressBar.java
+++ library/src/main/java/info/abdolahi/CircularMusicProgressBar.java
@@ -17,13 +17,14 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
-import android.support.annotation.ColorInt;
-import android.support.annotation.ColorRes;
-import android.support.annotation.DrawableRes;
-import android.support.v7.widget.AppCompatImageView;
+import androidx.annotation.ColorInt;
+import androidx.annotation.ColorRes;
+import androidx.annotation.DrawableRes;
 import android.util.AttributeSet;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+
+import androidx.appcompat.widget.AppCompatImageView;
 
 import info.abdolahi.circularmusicbar.R;
 
