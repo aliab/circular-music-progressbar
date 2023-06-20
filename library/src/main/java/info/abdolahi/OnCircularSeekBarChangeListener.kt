c5346a151c61fb569be7c49082723a20624a4a92
Mehdi Yari <mehdi.email.info@dgmail.com>
2023-06-20 19:26:12 +0200

    Migrate the OnCircularSeekBarChangeListener.java to Kotlin

--- library/src/main/java/info/abdolahi/OnCircularSeekBarChangeListener.kt
+++ library/src/main/java/info/abdolahi/OnCircularSeekBarChangeListener.kt
@@ -1,12 +1,11 @@
 package info.abdolahi;
-
 /**
  * A callback that notifies clients when the progress level has been changed.
  * This includes changes that were initiated by the user
  * through a touch gesture or arrow key/trackball as well as changes
  * that were initiated programmatically.
  */
-public interface OnCircularSeekBarChangeListener {
+interface  OnCircularSeekBarChangeListener {
 
     /**
      * Notification that the progress level has changed.
@@ -17,19 +16,19 @@ public interface OnCircularSeekBarChangeListener {
      * @param progress    : The current progress level. This will be in the range min.
      * @param fromUser    : True if the progress change was initiated by the user.
      */
-    void onProgressChanged(CircularMusicProgressBar circularBar, int progress, boolean fromUser);
+    fun onProgressChanged(circularBar: CircularMusicProgressBar?, progress: Int, fromUser: Boolean)
 
     /**
      * User click on circular bar
      *
      * @param circularBar : The CircularBar in which click happen
      */
-    void onClick(CircularMusicProgressBar circularBar);
+    fun onClick(circularBar: CircularMusicProgressBar?)
 
     /**
      * User long click on circular bar
      *
      * @param circularBar : The CircularBar in which long gesture happen
      */
-    void onLongPress(CircularMusicProgressBar circularBar);
+    fun onLongPress(circularBar: CircularMusicProgressBar?)
 }
